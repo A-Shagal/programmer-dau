78495e1c2a248b7b136fec318e67ff3ba06a4168
A-Shagal <126407544+A-Shagal@users.noreply.github.com>
2023-02-27 23:15:18 +0300

    Update Praktikum.java

--- Praktikum.java
+++ Praktikum.java
@@ -1,25 +1,25 @@
-    import java.util.Scanner;
-    public class Praktikum {
-        public static void main(String[] args) {
-            Scanner inputYear = new Scanner(System.in);
-            System.out.println("Введите год");
-            int year = inputYear.nextInt();
-            boolean a = LeapYearCalculator.isLeapYear(year);
-            if(year <= 0 || year >= 10000){
-                System.out.println("Ошибка, введите год от 1 до 9999");
-            } else if (a) {
-                System.out.println("Високосный год");
-            } else System.out.println("Обычный год");
-        }
+import java.util.Scanner;
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner inputYear = new Scanner(System.in);
+        System.out.println("Введите год");
+        int year = inputYear.nextInt();
+        boolean a = LeapYearCalculator.isLeapYear(year);
+        if(year <= 0 || year >= 10000){
+            System.out.println("Ошибка, некорректный ввод");
+        } else if (a) {
+            System.out.println("12.09."+ year + ". Високосный год");
+        } else System.out.println("13.09." + year + ".200 Обычный год");
     }
-    class LeapYearCalculator {
-        public static boolean isLeapYear(int year){
-            if(year <= 0 || year >= 10000 ){
-                return false;
-            } else if((year % 400 == 0 )){
-                return true;
-            }else if(year % 100 == 0){
-                return false;
-            }else return year % 4 == 0;
-        }
+}
+class LeapYearCalculator {
+    public static boolean isLeapYear(int year){
+        if(year <= 0 || year >= 10000 ){
+            return false;
+        } else if((year % 400 == 0 )){
+            return true;
+        }else if(year % 100 == 0){
+            return false;
+        }else return year % 4 == 0;
     }
+}
